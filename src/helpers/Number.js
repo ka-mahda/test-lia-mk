530c66ef4aedc9a50e4c8f774a7bab8eab81e551
ka-mahda <mahdakazemi1996@gmail.com>
2023-08-17 22:31:44 +0430

    add functions for numbers

---
+++ src/helpers/Number.js
@@ -0,0 +1,9 @@
+// export const convertToPersian = (number) => {
+//     const persian = number.replace(/\d/g, (d) => "۰۱۲۳۴۵۶۷۸۹"[d]);
+//     return persian;
+//   };
+  export function separateNumberByThousand(number) {
+    let newNumber = number && number?.toString()?.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    //newNumber = convertToPersian(newNumber);
+    return newNumber;
+  }
